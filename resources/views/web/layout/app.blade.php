4eec17ddf89246a64d83c9180efc0ed60071659e
Minhaj Haider Shah <smhaider1808@gmail.com>
2024-12-16 10:34:53 +0500

    nothing

--- resources/views/web/layout/app.blade.php
+++ resources/views/web/layout/app.blade.php
@@ -24,6 +24,9 @@
     <!-- Google font cdn  -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    {{-- <link rel="stylesheet" href="admin/assets/css/demo.css"> --}}
+    {{-- <link rel="stylesheet" href="admin/assets/vendor/css/theme-default.css"> --}}
+    {{-- <link rel="stylesheet" href="assets/vendor/css/core.css" class="template-customizer-core-css" />     --}}
     <link href="https://fonts.googleapis.com/css2?family=Bitter:ital,wght@0,100..900;1,100..900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 
